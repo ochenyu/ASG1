f1b15833e30ba2aedbedbece4eda443b7adcdf4e
ongchenyu@gmail.com <ongchenyu7@gmail.com>
2022-12-03 00:50:07 +0800

    added validation

---
+++ login.js
@@ -0,0 +1,81 @@
+const form = document.querySelector("form"),
+eField = form.querySelector(".email"),
+eInput = eField .querySelector("input"),
+pField = form.querySelector(".password"),
+pInput = pField.querySelector("input");
+
+
+// when blank
+form.onsubmit = (e) => {
+    e.preventDefault();
+    
+    if (eInput.value == "") {
+        eField.classList.add("shake", "error");
+    }
+    else {
+        validateEmail();
+    }
+
+    if(pInput.value == "") {
+        pField.classList.add("shake", "error");
+    }
+
+    setTimeout(()=>{
+        eField.classList.remove("shake");
+        pField.classList.remove("shake");}, 500);
+
+
+// input keyup
+eInput.onkeyup = () => {
+    validateEmail();    
+}   
+
+function validateEmail() {
+    let eformat = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!eInput.value.match(eformat)) {
+        eField.classList.add("error");
+        let errorTxt = eField.querySelector(".error-txt");
+        if (eInput.value != "") {
+            errorTxt.innerText = "Invalid email address"
+        }
+        else {
+            errorTxt.innerText = "Please fill in your email address"
+        }
+    }
+    else {
+        eField.classList.remove("error");
+    }
+}
+
+pInput.onkeyup = () => {
+    
+    if (pInput.value == "") {
+        pField.classList.add("error");
+    }
+    else {
+        pField.classList.remove("error");
+    }
+}
+
+// login successful
+if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+alert("Login successful")
+form.reset();
+}
+}
+
+
+// toggle show/hide
+function showPassword() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+
+
+
+
